088419ea9a0a7130ab854f6bb2e8d083620e5c72
dpweb <me@dpsw.info>
2012-12-10 14:08:59 -0600

    fix

--- fileup-node.js
+++ fileup-node.js
@@ -1,18 +1,18 @@
 module.exports = function(express, app, savedir, onupload){
 
 	var o = {}; o.log = function(){}
-	
+
 	var fs =require('fs');
 	try{fs.mkdirSync(savedir)} catch(e){}
-	
+
     app.use(express.bodyParser({uploadDir: savedir}));
-	
+
 	app.all('/upl', function(r,s){
 		s.send('<form method="post" enctype="multipart/form-data" action="/doupl"><input type="file" name="myfile"><input type="submit"></form>');
 	})
 	app.post('/doupl', function(r,s){
 			o.log('Getting file upload', r.files);
-			
+
 			fs.readFile(r.files.myfile.path, function (err, data) {
 				fs.writeFile(savedir+'/'+r.files.myfile.filename, data, function (err) {
 					 fs.unlink(r.files.myfile.path);
@@ -20,7 +20,7 @@ module.exports = function(express, app, savedir, onupload){
 				})
 			})
 	})
-	
+
 	return o;
 
 }
--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-	var express = require('express'), app = express();
+var express = require('express'), app = express();
 	var server = require('http').createServer(app)
  	server.listen(85);
 	console.log('listening on 85');
 
-	
+
  	var fileup = require('./fileup-node.js')(express, app, './files', function(r,s,f){
 		 s.end('saved file as ' + f.filename);
 	});
-	
+
 	// comment this for no logging
 	fileup.log = console.log;
-	
+
  	app.get('/', function(r,s){
  		s.send('example is at /upl')
  	})
-
